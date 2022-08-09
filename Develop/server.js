0896fae363a433101bc864768d05e6a343f5a354
Esmeralda Romero <esmeralda.r.a@hotmail.com>
2022-08-08 21:45:59 -0400

    notes can now be saved and seen on the left hand side

--- Develop/server.js
+++ Develop/server.js
@@ -26,8 +26,32 @@ app.get('/api/notes', (req, res) => //return index.html
 // POST request
 app.post('/api/notes', (req, res) => {
     // Let the client know that their POST request was received
-    res.json(`${req.method} request received`);
-  
+    res.json({requestBody: req.body})
+
+    let Note = req.body
+    let notes = []
+    fs.readFile('db/db.json', 'utf8', (err, data) => {
+        
+        if (err) {
+            console.log(`Error reading file from disk: ${err}`);
+        } else {
+    
+            // parse JSON string to JSON object
+            notes = JSON.parse(data);
+            notes.push(Note);
+            console.log(notes)
+
+            fs.writeFile('db/db.json', JSON.stringify(notes), err => {
+                if (err) {
+                  console.error(err);
+                }
+                // file written successfully
+            });
+              
+
+            //return notes
+        }});
+        res.json(`${req.method} request received`);
 });
 app.get('*', (req, res) => //return index.html
     res.sendFile(path.join(__dirname, 'public/index.html'))
